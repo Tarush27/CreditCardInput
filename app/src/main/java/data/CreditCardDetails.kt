040eb29619ae489c7f65c0ada1a067cfbf3f2e38
Tarushkaistha27 <tarushkaistha05@gmail.com>
2022-03-25 11:21:20 +0530

    removed unused CreditCardDetails data class.

--- app/src/main/java/data/CreditCardDetails.kt
+++
@@ -1,9 +0,0 @@
-package data
-
-data class CreditCardDetails(
-    private val creditCardNumber: String,
-    private val expirationDate: String,
-    private val cvv: String,
-    private val firstName: String,
-    private val lastName: String
-)
